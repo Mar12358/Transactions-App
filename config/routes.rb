827d51e89b9660bdfa2a054a37e067e36f3db37b
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-09-11 17:11:39 -0300

    Add resources to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
 
   # Defines the root path route ("/")
-  root "home#index"
+  authenticated :user do
+    # root 'categories#index', as: :authenticated_root
+    root "groups#index", as: :authenticated_root
+  end
+
+  unauthenticated do
+    root "home#index", as: :unauthenticated_root
+  end
+  resources :categories, only: [:index, :show, :new, :create, :destroy] do
+    resources :entities, only: [:index, :show, :new, :create, :destroy]
+  end
 end
