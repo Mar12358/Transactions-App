025cb90fefbb93ed2a11e4fbbb4027226086d156
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-09-11 13:22:58 -0300

    Allow home for not authenticated users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, unless: -> { home_page? || Rails.env.test? }
+
+  private
+
+  def home_page?
+    controller_name == 'home' && action_name == 'index'
+  end
 end
