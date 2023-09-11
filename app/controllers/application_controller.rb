877de1f9f7db1b6f222036de1f3120dea2e4073b
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-09-11 19:44:49 -0300

    Fix Linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  
   before_action :authenticate_user!, unless: -> { home_page? || Rails.env.test? }
   protect_from_forgery with: :exception
 
@@ -8,15 +7,12 @@ class ApplicationController < ActionController::Base
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
   end
-
 
   private
 
   def home_page?
     controller_name == 'home' && action_name == 'index'
   end
-
- 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ApplicationRecord
-  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :transactions, class_name: "Entity", foreign_key: :transaction_id
-  has_many :categories, class_name: "Group", foreign_key: :category_id
+  has_many :transactions, class_name: 'Entity', foreign_key: :transaction_id
+  has_many :categories, class_name: 'Group', foreign_key: :category_id
   # Add the :name attribute
 
   # Validate that the name field is not blank
