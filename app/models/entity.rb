3f088f3951f32ee3f2074894df680b9ddbb74dbf
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-09-11 17:10:24 -0300

    Add entities and groups

---
+++ app/models/entity.rb
@@ -0,0 +1,7 @@
+class Group < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :transactions, class_name: "Entity", foreign_key: :transaction_id
+  
+  # Validate that the name field is not blank
+  validates :name, presence: true
+end
