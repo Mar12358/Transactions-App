2b50df6790d0354189c3889f8f968713b11aec49
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-09-11 14:14:58 -0300

    Add name as a mandatory field when signing up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Add the :name attribute
+  attr_accessor :name
+
+  # Validate that the name field is not blank
+  validates :name, presence: true
 end
