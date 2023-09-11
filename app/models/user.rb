e58bd5bedd14f762cb12f16afd8152cebf73872c
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-09-11 17:10:58 -0300

    Add assosiations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :transactions, class_name: "Entity", foreign_key: :transaction_id
+  has_many :categories, class_name: "Group", foreign_key: :category_id
   # Add the :name attribute
   attr_accessor :name
 
