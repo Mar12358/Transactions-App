e25adf2d1f7abe57bde61545b198d5e00cfbd3f7
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-09-12 13:52:26 -0300

    Define methods and build views for Categories

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,3 +1,24 @@
 class GroupsController < ApplicationController
-  def index; end
+  def index
+    @categories = Group.includes(:transactions, :author).where(author: current_user)
+  end
+
+  def new
+    @category = Group.new
+  end
+
+  def create
+    @category = Group.new(post_params)
+    @category.author = current_user
+
+    if @category.save
+      redirect_to groups_path, notice: 'Category was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def post_params
+    params.require(:group).permit(:icon, :name)
+  end
 end
