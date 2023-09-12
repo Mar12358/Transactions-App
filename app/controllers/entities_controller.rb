8316840be94e8fd20265e90a5b13d12c6904be9f
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-09-12 14:37:06 -0300

    Navigate to transactions page and back.

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,2 +1,6 @@
 class EntitiesController < ApplicationController
+  def index
+    @category = Group.find(params[:group_id])
+    @transactions = @category.transactions.where(author: current_user)
+  end
 end
